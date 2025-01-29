8a204b6ae74fd96d590876ba07d867f4948dc4d5
MarcosFerranidz <marcos149ft@gmail.com>
2025-01-29 13:41:17 +0100

    Ejercicio 5 hecho, sigo sin enteder mucho esto

--- src/main/java/com/marcosferrandiz/tema06/Ejercicio5/Circunferencia.java
+++ src/main/java/com/marcosferrandiz/tema06/Ejercicio5/Circunferencia.java
@@ -1,4 +1,48 @@
 package com.marcosferrandiz.tema06.Ejercicio5;
 
 public class Circunferencia {
+
+    private punto centro;
+    private double radio;
+
+    public Circunferencia(punto centro, double radio) {
+        this.centro = centro;
+        this.radio = radio;
+    }
+
+    public Circunferencia(double x, double y, double radio){
+        this.centro = new punto(x,y);
+        this.radio = radio;
+    }
+    public Circunferencia(){
+        this(new punto(), 1.0);
+    }
+    public punto getCentro() {
+        return centro;
+    }
+    public void setCentro(punto centro) {
+        this.centro = centro;
+    }
+    public double getRadio() {
+        return radio;
+    }
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double distancia(punto punto){
+        return this.centro.distancia(punto);
+    }
+
+    public double area(){
+        return Math.PI * Math.pow(radio,2);
+    }
+    public double perimetro(){
+        return 2 * Math.PI * radio;
+    }
+
+    @Override
+    public String toString() {
+        return "Circunferencia{" + "centro=" + centro + ", radio=" + radio + '}';
+    }
 }
