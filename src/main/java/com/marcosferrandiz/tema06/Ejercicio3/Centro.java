6a621066303aa41fcb54fd8aae65da7a201a97c7
MarcosFerranidz <marcos149ft@gmail.com>
2025-02-26 00:26:48 +0100

    Ejercicio4, pero no va lo de la edad

--- src/main/java/com/marcosferrandiz/tema06/Ejercicio3/Centro.java
+++ src/main/java/com/marcosferrandiz/tema06/Ejercicio3/Centro.java
@@ -2,27 +2,140 @@ package com.marcosferrandiz.tema06.Ejercicio3;
 
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.Arrays;
 
 public class Centro {
     private int MAX_ALUMNOS = 99;
     private Alumno[] alumnos = new Alumno[MAX_ALUMNOS];
 
+    /**
+     * Crea un alumno nuevo con los parametros indicados, en el caso de que el NIA no este registrado devuelve true y lo crea, en el caso de que el NIA coincida devolvera false
+     * @param nia NIA del alumno
+     * @param nombre Nombre del alumno
+     * @param apellidos Apellidos del alumno (1 o 2 o 7)
+     * @param nacimiento Fecha de nacimiento del alumno
+     * @param grupo Grupo al que pertenece el alumno
+     * @param tel Telefono del alumno, padre o lo que sea
+     * @return Devuelve true si se ha creado el usuario y false si no se ha podido crear, ya sea porque el NIA se repite o porque esta lleno
+     */
     public boolean crearAlumno(int nia, String nombre, String apellidos, LocalDate nacimiento, String grupo, int tel){
-        for (int i = 0; i < alumnos.length ; i++) {
-            if ( alumnos[i] != null && alumnos[i].getNia() == nia){
-                System.out.println("Test");
-            }else {
-                for (int j = 0; i < alumnos.length  ; i++) {
-                    if (alumnos[j] == null){
-                        alumnos[j] = new Alumno(nia, nombre, apellidos, nacimiento, grupo, tel);
-                        System.out.println("Test2");
-                        return true;
-                    }
-                }
+        for (int i = 0; i < alumnos.length; i++) {
+            if (alumnos[i] != null && alumnos[i].getNia() == nia) {
+                return false;
+            }
+        }
+        for (int i = 0; i < alumnos.length; i++) {
+            if (alumnos[i] == null) {
+                alumnos[i] = new Alumno(nia, nombre, apellidos, nacimiento, grupo, tel);
+                return true;
             }
         }
         return false;
+    }
+
+    /**
+     * Busca el NIA introducido y devuelve true si lo encuentra y false si no existe
+     * @param nia NIA que debe de buscar
+     * @return Devuelve true si ha encontrado el NIA y false si no existe el NIA ese
+     */
+    public boolean buscarNia(int nia){
+        for (int i = 0; i < alumnos.length ; i++) {
+            if (alumnos[i] != null && alumnos[i].getNia() == nia) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Da de baja a un alumno y te devuelve true si se ha hecho de forma correcta o false si no se ha podido hacer porque no existe nadie con el NIA
+     * @param nia Es el NIA del alumno que queremos dar de baja
+     * @return Devuelve true si se ha hecho de forma buena o false si no se podido realizar
+     */
+    public boolean bajaAlumno(int nia){
+        for (int i = 0; i < alumnos.length ; i++) {
+            if (alumnos[i] != null && alumnos[i].getNia() == nia) {
+                alumnos[i] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Crea un array de tipo Alumno donde guardaremos los alumnos que estan en el grupo
+     * @param grupo Es el grupo que queremos consultar
+     * @return Devuelve un array de tipo Alumno en el cual estan los alumnos del grupo que buscamos
+     */
+    public Alumno[] alumnosPorGrupo(String grupo){
+        int contador = 0;
+        int posicionParaGrupo = 0;
+        for (int i = 0; i < alumnos.length; i++) {
+            if (alumnos[i] != null && alumnos[i].getGrupo().equals(grupo)) {
+                contador++;
+            }
+        }
+        Alumno[] alumnosGrupo = new Alumno[contador];
+        for (int i = 0; i < alumnos.length; i++) {
+            if (alumnos[i] != null && alumnos[i].getGrupo().equals(grupo)) {
+                alumnosGrupo[posicionParaGrupo++] = alumnos[i];
+            }
+        }
+        return alumnosGrupo;
+    }
+
+    public int edad(int index){
+        return Period.between(alumnos[index].getNacimiento(), LocalDate.now()).getYears();
+    }
+
+    //No consigo hacer que funcione conio
+    public Alumno[] alumnosPorEdad(int edad){
+        int contador = 0;
+        int posicionParaArrayEdad = 0;
+        for (int i = 0; i < alumnos.length ; i++) {
+            if (alumnos[i] != null && edad(i) == edad) {
+                contador++;
+            }
+        }
+        Alumno[] alumnosEdad = new Alumno[contador];
+        for (int i = 0; i < alumnos.length ; i++) {
+            if (alumnos[i] != null && edad(i) == edad);{
+                alumnosEdad[posicionParaArrayEdad++] = alumnos[i];
+            }
+        }
+        return alumnosEdad;
+    }
+
+    /**
+     * Metodo que busca un alumno por su NIA
+     * @param nia Es el NIA del alumno que estamos buscando
+     * @return Devuelve un Array de Alumno con la informacion del alumno con el NIA, si no existe pues devuelve null
+     */
+    public Alumno[] alumnoPorNia(int nia){
+        for (int i = 0; i < alumnos.length ; i++) {
+            if (alumnos[i] != null && alumnos[i].getNia() == nia) {
+                return new Alumno[]{alumnos[i]};
+            }
+        }
+        return null;
+    }
+
+    public Alumno[] alumnoPorApellidos(String apellidos){
+        int contador = 0;
+        int posicionParaArrayApellidos = 0;
+        for (int i = 0; i < alumnos.length ; i++) {
+            if (alumnos[i] != null && alumnos[i].getApellidos().equals(apellidos)) {
+                contador++;
+            }
+        }
+        Alumno[] alumnosApellidos = new Alumno[contador];
+        for (int i = 0; i < alumnos.length ; i++) {
+            if (alumnos[i] != null && alumnos[i].getApellidos().equals(apellidos)) {
+                alumnosApellidos[posicionParaArrayApellidos++] = alumnos[i];
+            }
+        }
+        return alumnosApellidos;
     }
 
     @Override
--- src/main/java/com/marcosferrandiz/tema06/Ejercicio3/Ejercicio3.java
+++ src/main/java/com/marcosferrandiz/tema06/Ejercicio3/Ejercicio3.java
@@ -3,11 +3,21 @@ package com.marcosferrandiz.tema06.Ejercicio3;
 import com.marcosferrandiz.tema06.libreria.solicitarCosas;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
+
+import com.github.javafaker.Faker;
+
+
 
 public class Ejercicio3 {
 
     private static Centro centro = new Centro();
 
+    /**
+     * Metodo que genera el menu principal
+     */
     public static void principalMenu(){
         int seleccion = 0;
         do {
@@ -17,24 +27,77 @@ public class Ejercicio3 {
             System.out.println("1. Nuevo Alumno …");
             System.out.println("2. Baja de Alumno …");
             System.out.println("3. Consultas …");
+            System.out.println("4. Generar con Faker …");
+            System.out.println("5. Ver …");
             System.out.println("------------------------------");
             System.out.println("0. Salir");
-            seleccion = solicitarCosas.solicitarEntero("Seleccione la opción deseada",0,3);
+            seleccion = solicitarCosas.solicitarEntero("Seleccione la opción deseada",0,5);
 
-            if (seleccion <0 || seleccion >3){
-                System.err.println("El valor introducido no es valido");
-            }
             switch (seleccion){
+                case 0 -> System.out.println("Saliendo...");
                 case 1 -> crearAlumno();
-                //case 2 ->;
-                //case 3 -> ;
-                //default -> /*Hola ;
+                case 2 -> darDeBajaAlumno();
+                case 3 -> consultasMenu();
+                case 4 -> generarAlumnosConFaker(5);
+                case 5 -> System.out.println(centro);
+                default -> System.err.println("Opcion no valida");
             }
         }while (seleccion != 0);
+    }
 
+    public static void consultasMenu(){
+        int seleccion = 0;
+        do {
+            System.out.println("***************");
+            System.out.println("** CONSULTAS **");
+            System.out.println("***************");
+            System.out.println("1. Por grupo ...");
+            System.out.println("2. Por edad ...");
+            System.out.println("3. Por nia ...");
+            System.out.println("4. Por apellidos ...");
+            System.out.println("--------------------");
+            System.out.println("0. Volver al menú principal");
+            seleccion = solicitarCosas.solicitarEntero("Seleccione la opción deseada",0,4);
+            if (seleccion <0 || seleccion >4){
+                System.err.println("El valor introducido no es valido");
+            }
+
+            switch (seleccion){
+                case 0 -> principalMenu();
+                case 1 -> mostrarAlumnosDeGrupo();
+                case 2 -> mostrarAlumnosPorEdad();
+                case 3 -> mostrarAlumnosPorNia();
+                case 4 -> mostrarAlumnosPorApellidos();
+            }
+
+        }while (seleccion != 0);
 
     }
 
+    public static void generarAlumnosConFaker(int cantidad){
+        Faker faker = new Faker();
+        for (int i = 0; i <= cantidad ; i++) {
+            int nia = faker.number().numberBetween(10000000,99999999);
+            String nombre = faker.name().name();
+            String apellido = faker.name().lastName();
+
+            //Aqui he tirado de chatGPT porque no tenia ni idea de como funciona faker ni como pasar un date que es lo que te da a un LocalDate
+            LocalDate hoy = LocalDate.now();
+            LocalDate hace100Anios = hoy.minusYears(100);
+            Date fechNac = faker.date().between(Date.from(hace100Anios.atStartOfDay(ZoneId.systemDefault()).toInstant()), Date.from(hoy.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+            LocalDate nacimiento = fechNac.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            //--------------------------------------------------------------------------------------------------
+
+            String grupo = faker.options().option(new String[]{"1DAM","2DAM","1DAW","2DAW"});
+            int telEnInt = faker.number().numberBetween(10000000,99999999);
+
+            centro.crearAlumno(nia,nombre,apellido,nacimiento,grupo,telEnInt);
+        }
+    }
+
+    /**
+     * Metodo que da de alta a un alumno
+     */
     public static void crearAlumno(){
         int nia;
         String nombre;
@@ -53,17 +116,107 @@ public class Ejercicio3 {
             crearAlumnoVerificacion = centro.crearAlumno(nia,nombre,apellido,fechaNac,grupo,tel);
             System.out.println(crearAlumnoVerificacion);
             if (!crearAlumnoVerificacion){
-                System.err.println("Error, el NIA ese ya esta seleccionado en otro alumno");
+                System.err.println("Error, el NIA ese ya esta seleccionado en otro alumno o esta lleno el array de alumnos");
             }else {
                 System.out.println("Se ha creado correctamente");
             }
         }while (!crearAlumnoVerificacion);
     }
 
+
+    /**
+     * Metodo que da de baja a un alumno
+     */
+    public static void darDeBajaAlumno(){
+        char deci;
+        int nia = solicitarCosas.solicitarInt("Introduzca el NIA del alumno a dar de baja: ",0);
+        boolean niaVerificado = centro.buscarNia(nia);
+        if (niaVerificado){
+            deci = solicitarCosas.solicitarCaracter("Esta seguro de que desea eliminar al alumno con el NIA "+ nia+" ( S | N )");
+            if (deci == 'S' || deci == 's'){
+                centro.bajaAlumno(nia);
+                System.out.println("Se ha dado de baja de forma correcta");
+            }
+            else {
+                System.out.println("Si es que hay que pensarselo mejor hombre");
+            }
+        }
+        else {
+            System.err.println("Ese nia no existe, confirme que lo ha escrito bien o que el alumno exista");
+        }
+    }
+
+    /**
+     * Metodo que muestra los alumnos de un grupo que le diremos
+     */
+    public static void mostrarAlumnosDeGrupo(){
+        String grupo = solicitarCosas.solicitarStringSinLimite("Introduzca el grupo que desea consultar: ");
+        Alumno[] alumnosGrupo = centro.alumnosPorGrupo(grupo);
+        boolean tieneAlumnosDentro = false;
+
+        for (int i = 0; i < alumnosGrupo.length; i++) {
+            if (alumnosGrupo[i] != null) {
+                tieneAlumnosDentro = true;
+                System.out.println(alumnosGrupo[i].toString());
+            }
+        }
+
+        if (!tieneAlumnosDentro) {
+            System.err.println("No hay alumnos en este grupo o no existe el grupo.");
+        }
+    }
+
+    //No lo consigo hacer que funcione conio
+    public static void mostrarAlumnosPorEdad(){
+        int edad = solicitarCosas.solicitarInt("Introduzca la edad que desea consultar: ",0);
+        Alumno[] alumnosEdad = centro.alumnosPorEdad(edad);
+        boolean tieneAlumnosDentro = false;
+
+        for (int i = 0; i < alumnosEdad.length ; i++) {
+            if (alumnosEdad[i] != null){
+                tieneAlumnosDentro = true;
+                System.out.println(alumnosEdad[i].toString());
+            }
+        }
+        if (!tieneAlumnosDentro){
+            System.err.println("No hay alumnos que contengan esa edad");
+        }
+    }
+
+    /**
+     * Muestra el alumna con el NIA seleccionado
+     */
+    public static void mostrarAlumnosPorNia(){
+        int nia = solicitarCosas.solicitarInt("Introduzca el NIA que desea consultar: ",0);
+        boolean niaVerificado = centro.buscarNia(nia);
+        Alumno[] alumnoNia = centro.alumnoPorNia(nia);
+        if (niaVerificado){
+            System.out.println(Arrays.toString(alumnoNia));
+        }
+        else {
+            System.err.println("No existe *Carita triste*");
+        }
+    }
+
+    /**
+     * Muestra la consulta de los alumnos con el apellido que introduciremos despues
+     */
+    public static void mostrarAlumnosPorApellidos(){
+        String apellidos = solicitarCosas.solicitarStringSinLimite("Introduzca los apellidos: ");
+        Alumno[] alumnosApellidos = centro.alumnoPorApellidos(apellidos);
+        boolean tieneAlumnosDentro = false;
+        for (int i = 0; i < alumnosApellidos.length ; i++) {
+            if (alumnosApellidos[i] != null){
+                tieneAlumnosDentro = true;
+                System.out.println(alumnosApellidos[i].toString());
+            }
+        }
+        if (!tieneAlumnosDentro){
+            System.err.println("No hay alumnos con esos apellidos");
+        }
+    }
+
     public static void main(String[] args) {
-        crearAlumno();
-        System.out.println(centro.toString());
-        crearAlumno();
-        System.out.println(centro.toString());
+        principalMenu();
     }
 }
